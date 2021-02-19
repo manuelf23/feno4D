2b63c4287217139f70b8d63bc5c21e914ff0b304
Manuel Felipe Garcia Rincon <manuelf.garcia23@gmail.com>
2021-02-18 22:26:59 -0500

    se crea una carpeta con la fecha y se guarda la data

--- capture_data_feno4d.py
+++ capture_data_feno4d.py
@@ -1,7 +1,34 @@
 from read_frames_lidar import save_lidar_csv_file
+import os
+from datetime import datetime
+now = datetime.now()
+dt_string = now.strftime("%d-%m-%Y--%H-%M-%S")
+base_path = './' + dt_string
 
-fileName = '0005'
 num_psckets = 1
 net_interface = 'en6'
-r = save_lidar_csv_file(fileName, num_psckets, net_interface)
-print(r)
+STEPS_PER_REV_BASE = 1300
+STEPS_PER_REV_MOTOR = 96
+
+angle = 90
+no_toma_data = 1
+
+step_number_per_revolution = int(360/angle)
+step_number_per_angle = int(STEPS_PER_REV_BASE/step_number_per_revolution)
+
+try:
+    os.mkdir(base_path)
+except OSError:
+    print ("Creation of the directory %s failed" % base_path)
+else:
+    print ("Successfully created the directory %s " % base_path)
+
+for rep in range(step_number_per_revolution):
+    fileName = base_path + '/' + str(rep+1)
+    print('parrot images saved (NO)')
+    r = save_lidar_csv_file(fileName, num_psckets, net_interface)
+    if r ==1 :
+        print('LiDAR frames saved (yes)')
+        sense_step(step_number_per_angle)
+
+print('TOMA DE DATA FINALIZADA')
