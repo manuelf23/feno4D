a8571d91cee4282f88941ae88d26e57baa208205
Manuel Felipe Garcia Rincon <manuelf.garcia23@gmail.com>
2021-03-04 19:06:49 -0500

    motor con arduino

---
+++ ardu_motor.py
@@ -0,0 +1,31 @@
+import Jetson.GPIO as GPIO
+import time
+
+RX_JETSON = 18
+TX_JETSON = 24
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(TX_JETSON, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(RX_JETSON, GPIO.IN)
+
+paso_base = 30
+paso = 0
+
+def medir():
+    print('midiendo')
+    time.sleep(1)
+    print('fin de medicion')
+
+while(1):
+    medir()
+    GPIO.output(TX_JETSON, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(TX_JETSON, GPIO.LOW)
+    time.sleep(0.5)
+    rotado = GPIO.input(RX_JETSON)
+    while (not GPIO.input(RX_JETSON)):
+        pass
+    paso += paso_base
+    if paso == 360:
+        input('Vuelta completa. ENTER PARA CONTINUAR:')
